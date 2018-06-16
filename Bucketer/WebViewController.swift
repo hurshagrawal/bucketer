7cd53dd199c923a010843082853276b5144d5ebe
Hursh Agrawal <h@hurshagrawal.com>
2018-06-16 17:50:29 +0200

    Add bitbucket css

--- Bucketer/WebViewController.swift
+++ Bucketer/WebViewController.swift
@@ -6,6 +6,7 @@ import WebKit
 
 class WebViewController: UIViewController, WKNavigationDelegate {
   var webView: WKWebView!
+  var cssString: String!
 
   override func loadView() {
     webView = WKWebView()
@@ -16,14 +17,23 @@ class WebViewController: UIViewController, WKNavigationDelegate {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    if let path = Bundle.main.path(forResource: "bitbucket", ofType: "css") {
+      do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        let string = String(data: data, encoding: String.Encoding.utf8) as String!
+        self.cssString = string?.replacingOccurrences(of: "\n", with: "")
+      } catch {
+        print("[ERROR] Could not find bitbucket.css")
+      }
+    }
+
     let url = URL(string: "https://bitbucket.org/dashboard/overview")
     webView.load(URLRequest(url: url!))
     webView.allowsBackForwardNavigationGestures = true
   }
 
   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    let css = ""
-    let js = "var style = document.createElement('style'); style.innerHTML = '\(css)'; document.head.appendChild(style);"
+    let js = "var style = document.createElement('style'); style.innerHTML = '\(cssString!)'; document.head.appendChild(style);"
     webView.evaluateJavaScript(js, completionHandler: nil)
   }
 }
